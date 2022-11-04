50187904860ea45336db2d39512e5dcea11b9267
Dragonprod <artyom2002-98@mail.ru>
2022-11-04 22:19:59 +0300

    fix: update models

--- app/models/apartments.py
+++ app/models/apartments.py
@@ -1,25 +1,26 @@
 from decimal import Decimal
 from typing import Optional
 
-from pydantic import UUID4, BaseModel, Field
+from pydantic import UUID4, BaseModel, Field, HttpUrl
 
 from app.models.utils import optional
 
 
 class ApartmentBase(BaseModel):
     address: str = Field(description="Адрес квартиры")
+    link: Optional[HttpUrl] = Field(description="Ссылка на объявление")
     lat: Optional[Decimal] = Field(description="Широта")
     lon: Optional[Decimal] = Field(description="Долгота")
     rooms: int = Field(description="Количество комнат")
     segment: str = Field(description="Тип жилья")
     floors: int = Field(description="Количество этажей")
-    walls: str = Field(description="Материал стен")
+    walls: Optional[str] = Field(description="Материал стен")
     floor: int = Field(description="Этаж")
     apartment_area: int = Field(description="Площадь квартиры")
-    kitchen_area: int = Field(description="Площадь кухни")
-    has_balcony: bool = Field(description="Наличие балкона")
-    distance_to_metro: int = Field(description="Расстояние до метро")
-    quality: str = Field(description="Отделка")
+    kitchen_area: Optional[int] = Field(description="Площадь кухни")
+    has_balcony: Optional[bool] = Field(description="Наличие балкона")
+    distance_to_metro: Optional[int] = Field(description="Расстояние до метро")
+    quality: Optional[str] = Field(description="Отделка")
     m2price: Optional[Decimal] = Field(0, description="Цена за квадратный метр")
     price: Optional[int] = Field(0, description="Цена квартиры")
 
